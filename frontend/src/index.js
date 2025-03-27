f4ff6921c3c4b899b49906daecc6eaa3000f8cdf
Abdul Alkawasmeh <114367378+AbdulAlkawasmeh@users.noreply.github.com>
2025-03-27 16:51:05 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,8 @@ import CreatePosts from "./pages/CreatePosts";
 import TenantPage from "./pages/TenantPage"; 
 import "./index.css"; 
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";  // Ensure 'Route' and 'Routes' are imported
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
