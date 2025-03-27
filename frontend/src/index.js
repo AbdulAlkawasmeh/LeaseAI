78422b9ffee9a77fa04a4c403e328cfdc2743653
Abdul Alkawasmeh <114367378+AbdulAlkawasmeh@users.noreply.github.com>
2025-03-27 15:44:07 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom/client'; 
 import Home from "./pages/Home"; 
 import "./index.css"; 
 import reportWebVitals from './reportWebVitals';
