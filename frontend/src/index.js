dd2aa5c89398866b35ca686143bcee6ee9e02f58
Abdul Alkawasmeh <114367378+AbdulAlkawasmeh@users.noreply.github.com>
2025-03-27 16:49:19 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'; 
 import Home from "./pages/Home"; 
+import CreatePosts from "./pages/CreatePosts";
+import TenantPage from "./pages/TenantPage"; 
 import "./index.css"; 
 import reportWebVitals from './reportWebVitals';
 
@@ -8,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Home />
+  <Route path="/create-posts" component={CreatePosts} />
   </React.StrictMode>
 );
 
