3dfe7e46212de3408ca9c3b9343ec26ea21b90b0
Abdul Alkawasmeh <114367378+AbdulAlkawasmeh@users.noreply.github.com>
2025-03-27 16:22:11 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';  // Import Router
 import Home from "./pages/Home"; 
+import CreatePosts from "./pages/CreatePosts";  // Import CreatePosts page
+import TenantPage from "./pages/TenantPage";  // Import TenantPage
 import "./index.css"; 
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
+    <Router>  {/* Wrap the app in Router */}
+      <Switch>  {/* Switch ensures only the first matching route is rendered */}
+        <Route exact path="/" component={Home} />  {/* Route for Home page */}
+        <Route path="/create-posts" component={CreatePosts} />  {/* Route for CreatePosts page */}
+        <Route path="/tenant-page" component={TenantPage} />  {/* Route for TenantPage */}
+        {/* Add more routes as needed */}
+      </Switch>
+    </Router>
   </React.StrictMode>
 );
 
