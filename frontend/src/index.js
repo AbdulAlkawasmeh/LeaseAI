25d3ea908fd6fcc54fffba965171b5a8758ccc53
Abdul Alkawasmeh <114367378+AbdulAlkawasmeh@users.noreply.github.com>
2025-03-27 16:55:25 -0400

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'; 
-import Home from "./pages/Home"; 
-import CreatePosts from "./pages/CreatePosts";
-import TenantPage from "./pages/TenantPage"; 
+import App from "./App"; 
 import "./index.css"; 
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";  // Ensure 'Route' and 'Routes' are imported
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Home />
-  <Route path="/create-posts" component={CreatePosts} />
+    <App />
   </React.StrictMode>
 );
 
